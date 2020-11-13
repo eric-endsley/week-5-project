c9017cfd2ed352d598d0d6c211005cd5f77ca3c9
Eric Endsley <eric.endsley4@gmail.com>
2020-11-13 10:38:38 -0800

    fix test syntax and intentionally fail first test

--- _tests_/spageAge.test.js
+++ _tests_/spageAge.test.js
@@ -1,10 +1,10 @@
 import { TestScheduler } from 'jest';
-import { SpaceAge } from ../src/js/spaceAge.js
+import { SpaceAge } from '../src/js/spaceAge.js'
 
-Describe ('SpaceAge', () => {
+describe ('SpaceAge', () => {
   test('should construct a SpaceAge object with property age equal to age parameter', () => {
 
   let spaceAge = new SpaceAge(20);
-  expect(spaceAge.age.toEqual(20))
+  expect(spaceAge.age).toEqual(19)
   });
 });
