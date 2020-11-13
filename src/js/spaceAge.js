4dc47f1833b8b22131f503206088e2bf54220ded
Eric Endsley <eric.endsley4@gmail.com>
2020-11-13 11:43:57 -0800

    write functionality for constructor to calculate ages on venus, mars and jupiter given age param

--- src/js/spaceAge.js
+++ src/js/spaceAge.js
@@ -2,8 +2,8 @@ export class SpaceAge {
   constructor(age) {
     this.age = age;
     this.mercuryAge = Math.round(age / 0.24);
-    this.venusAge = 0;
-    this.marsAge = 0;
-    this.jupiterAge = 0;
+    this.venusAge = Math.round(age / 0.62);
+    this.marsAge = Math.round(age / 1.88);
+    this.jupiterAge = Math.round(age / 11.86);
   }
 };
