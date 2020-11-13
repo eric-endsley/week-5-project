fd2568e4147455a6dea85600c9cb597dabbba0a9
Eric Endsley <eric.endsley4@gmail.com>
2020-11-13 10:57:46 -0800

    intentionally fail second test

--- _tests_/spaceAge.test.js
+++ _tests_/spaceAge.test.js
@@ -2,17 +2,17 @@ import { TestScheduler } from 'jest';
 import { SpaceAge } from '../src/js/spaceAge.js'
 
 describe ('SpaceAge', () => {
+  let spaceAge
   beforeEach(() => {
-    let spaceAge = new SpaceAge(20);
+     spaceAge = new SpaceAge(20);
   });
   test('should construct a SpaceAge object with property age equal to age parameter', () => {
   expect(spaceAge.age).toEqual(20)
   });
   test('should construct a SpaceAge object with properties mercuryAge, venusAge, marsAge & jupiterAge equal to 0', () => {
-  expect(spaceAge.mercuryAge).toEqual(0)
-  expect(spaceAge.venusAge).toEqual(0)
-  expect(spaceAge.marsAge).toEqual(0)
-  expect(spaceAge.jupiterAge).toEqual(0)
+  expect(spaceAge.mercuryAge).toEqual(1)
+  expect(spaceAge.venusAge).toEqual(1)
+  expect(spaceAge.marsAge).toEqual(1)
+  expect(spaceAge.jupiterAge).toEqual(1)
   });
-
 });
