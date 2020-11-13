1b9c0367793a4251b29951f53ca43c7f5557a364
Eric Endsley <eric.endsley4@gmail.com>
2020-11-13 10:58:45 -0800

    intentionally pass second test, spaceAge constructor sets plant age properties equal to 0

--- _tests_/spaceAge.test.js
+++ _tests_/spaceAge.test.js
@@ -10,9 +10,9 @@ describe ('SpaceAge', () => {
   expect(spaceAge.age).toEqual(20)
   });
   test('should construct a SpaceAge object with properties mercuryAge, venusAge, marsAge & jupiterAge equal to 0', () => {
-  expect(spaceAge.mercuryAge).toEqual(1)
-  expect(spaceAge.venusAge).toEqual(1)
-  expect(spaceAge.marsAge).toEqual(1)
-  expect(spaceAge.jupiterAge).toEqual(1)
+  expect(spaceAge.mercuryAge).toEqual(0)
+  expect(spaceAge.venusAge).toEqual(0)
+  expect(spaceAge.marsAge).toEqual(0)
+  expect(spaceAge.jupiterAge).toEqual(0)
   });
 });
