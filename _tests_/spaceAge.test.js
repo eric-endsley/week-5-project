d17deed93f631634bc4a332b4800228a28d7b0ee
Eric Endsley <eric.endsley4@gmail.com>
2020-11-13 10:53:28 -0800

    write second test for SpaceAge constructor, expect new planet age properties to equal 0

--- _tests_/spaceAge.test.js
+++ _tests_/spaceAge.test.js
@@ -5,9 +5,14 @@ describe ('SpaceAge', () => {
   beforeEach(() => {
     let spaceAge = new SpaceAge(20);
   });
-
   test('should construct a SpaceAge object with property age equal to age parameter', () => {
   expect(spaceAge.age).toEqual(20)
+  });
+  test('should construct a SpaceAge object with properties mercuryAge, venusAge, marsAge & jupiterAge equal to 0', () => {
+  expect(spaceAge.mercuryAge).toEqual(0)
+  expect(spaceAge.venusAge).toEqual(0)
+  expect(spaceAge.marsAge).toEqual(0)
+  expect(spaceAge.jupiterAge).toEqual(0)
   });
 
 });
