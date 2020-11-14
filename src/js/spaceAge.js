109f8432af80a189313a952fe642a6e5b5835789
Eric Endsley <eric.endsley4@gmail.com>
2020-11-13 16:47:40 -0800

    write lifeExpect method to return string stating how many years a user has left to live on given planet

--- src/js/spaceAge.js
+++ src/js/spaceAge.js
@@ -5,14 +5,16 @@ export class SpaceAge {
     this.marsConst = 1.88;
     this.jupiterConst = 11.86;
   }
+
   spaceAgeCalc(age, planet) {
     let planetAge = Math.round(age / this[planet + "Const"]);
     let planetToCap = planet.charAt(0).toUpperCase() + planet.slice(1);
     return `Since you are ${age} on Earth, you would be ${planetAge} on ${planetToCap}.`;
   }
 
-//   lifeExpect(lifeSpan, planet) {
-//     console.log(this[${planet}Age])
-//     lifeSpan - this[planet + Age]
-//   }
+  lifeExpect(age, lifeSpan, planet) {
+    let planetLifeExpect = Math.round(lifeSpan - age / this[planet + "Const"]);
+    let planetToCap = planet.charAt(0).toUpperCase() + planet.slice(1);
+    return `You should have ${planetLifeExpect} years left to live on ${planetToCap}.`;
+  }
 };
