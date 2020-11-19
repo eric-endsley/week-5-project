193104791682c170fdd84b46864ce87b7c1db9c8
Eric Endsley <eric.endsley4@gmail.com>
2020-11-18 21:44:37 -0800

    write second test for lifeExpect

--- _tests_/spaceAge.test.js
+++ _tests_/spaceAge.test.js
@@ -38,4 +38,7 @@ describe ('lifeExpect', () => {
     expect(spaceAge.lifeExpect(20, 75, "mars")).toEqual("You should have 29 years left to live on Mars.")
     expect(spaceAge.lifeExpect(20, 75, "jupiter")).toEqual("You should have 5 years left to live on Jupiter.")
   });
+    test('should return a string which states how many years a person has lived beyond their average life expectancy on a given planet if they have done so, rounded to the nearest integer', () => {
+      expect(spaceAge.lifeExpect(80, 75, "mercury")).toEqual("You have lived 21 years passed your life expectancy in Mercury years")
+  });
 });
